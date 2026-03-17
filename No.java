05f806808f647975ac9c8992e1a211e3976782be
Djan Gomes Tavares <dgt1000@gmail.com>
2026-03-17 20:29:02 -0300

    createf class No

---
+++ No.java
@@ -0,0 +1,23 @@
+package fila;
+
+public class No {
+	private int dado;
+	private No proximo;
+	
+	public int getDado() {
+		return dado;
+	}
+	
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+	
+	public No getProximo() {
+		return proximo;
+	}
+	
+	public void setProximo(No proximo) {
+		this.proximo = proximo;
+	}
+	
+}
