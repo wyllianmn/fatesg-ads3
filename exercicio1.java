00317ac0e51483aadadf2f4a3684d017b4205d7e
JurandirNeto <jurandirnetosenai@gmail.com>
2026-03-17 21:25:01 -0300

    Exercicio1

---
+++ exercicio1.java
@@ -0,0 +1,28 @@
+public class exercicio1 {
+    
+    private No cabeca;
+
+    public void inserirNoInicio(int valor) {
+        No novoNo = new No();
+        novoNo.setDado(valor);
+        
+        novoNo.setProximo(cabeca);
+        cabeca = novoNo;
+    }
+
+    public void inserirNoFinal(int valor) {
+        No novoNo = new No();
+        novoNo.setDado(valor);
+
+        if (cabeca == null) {
+            cabeca = novoNo;
+            return;
+        }
+
+        No atual = cabeca;
+        while (atual.getProximo() != null) {
+            atual = atual.getProximo();
+        }
+        atual.setProximo(novoNo);
+    }
+}
