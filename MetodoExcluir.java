d2fe6457b8d59e87075d5638ff5effe0c887267f
Lucio Aguiar de Souza <lucioaguiardesouza@gmail.com>
2026-03-17 21:05:22 -0300

    Feet Excluir
    Implementado o Metodo Excluir, onde permita remover um número específico da lista encadeada. Caso o número não exista na lista, a estrutura deve permanecer inalterada.

---
+++ MetodoExcluir.java
@@ -0,0 +1,39 @@
+package fila;
+
+/**
+ * Autores:
+
+  Lucio Aguiar de Souza, 
+  Felipe Estevão, 
+  Marco Túlio Costa, 
+  Gustavo Fernando
+
+ * 
+ */
+
+public class MetodoExcluir {
+
+    private No inicio;
+
+    public void excluir(int valor) {
+        if (inicio == null) {
+            return;
+        }
+
+        if (inicio.getDado() == valor) {
+            inicio = inicio.getProximo();
+            return;
+        }
+
+        No atual = inicio;
+
+        while (atual.getProximo() != null && atual.getProximo().getDado() != valor) {
+            atual = atual.getProximo();
+        }
+
+        if (atual.getProximo() != null) {
+            atual.setProximo(atual.getProximo().getProximo());
+        }
+    }
+
+}
